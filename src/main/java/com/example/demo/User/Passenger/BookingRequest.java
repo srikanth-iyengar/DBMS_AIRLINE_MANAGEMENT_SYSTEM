009c6a1670e7a3633d827956ba7e90cb69e89515
Srikanth Iyengar <ksrikanth3012@gmail.com>
2022-07-15 01:06:26 +0530

    added

--- src/main/java/com/example/demo/User/Passenger/BookingRequest.java
+++ src/main/java/com/example/demo/User/Passenger/BookingRequest.java
@@ -4,13 +4,7 @@ import java.util.List;
 
 import com.example.demo.Booking.PaymentMode;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
 public class BookingRequest {
 	private Long flightid;
 	private Long userid;
